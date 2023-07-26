197a920132ff79f9681437594817b75a5320e499
Jesse White <jesse.white.oss@gmail.com>
2023-07-25 23:26:51 -0400

    barely working deps

--- pkg/assembler/backends/tinkerpop/isDependency.go
+++ pkg/assembler/backends/tinkerpop/isDependency.go
@@ -3,11 +3,18 @@ package tinkerpop
 import (
 	"context"
 	"fmt"
+	gremlingo "github.com/apache/tinkerpop/gremlin-go/v3/driver"
 	"github.com/guacsec/guac/pkg/assembler/graphql/model"
+	"strconv"
 )
 
-func getDependencyQueryValues(pkg *model.PkgInputSpec, depPkg *model.PkgInputSpec, dependency *model.IsDependencyInputSpec) map[string]any {
-	values := map[string]any{}
+const (
+	IsDependency Label = "isDependency"
+)
+
+func getDependencyQueryValues(pkg *model.PkgInputSpec, depPkg *model.PkgInputSpec, dependency *model.IsDependencyInputSpec) map[interface{}]interface{} {
+	values := make(map[interface{}]interface{})
+	values[gremlingo.T.Label] = string(IsDependency)
 
 	// add guac keys
 	pkgId := guacPkgId(*pkg)
@@ -26,15 +33,60 @@ func getDependencyQueryValues(pkg *model.PkgInputSpec, depPkg *model.PkgInputSpe
 	return values
 }
 
+func getDependencyObject(id int64, values map[interface{}]interface{}) *model.IsDependency {
+	isDependency := &model.IsDependency{
+		ID: strconv.FormatInt(id, 10),
+		//Package:          pkg,
+		//DependentPackage: depPkg,
+		//VersionRange:     createdValue.VersionRange,
+		//DependencyType:   dependencyTypeEnum,
+		Justification: values[justification].(string),
+		Origin:        values[collector].(string),
+		Collector:     values[origin].(string),
+	}
+	return isDependency
+}
+
 func (c *tinkerpopClient) IngestDependency(ctx context.Context, pkg model.PkgInputSpec, depPkg model.PkgInputSpec, dependency model.IsDependencyInputSpec) (*model.IsDependency, error) {
-	getDependencyQueryValues(&pkg, &depPkg, &dependency)
-	//TODO implement me
-	panic("implement me")
+	pkgVertexProperties := getPackageQueryValues(&pkg)
+	depPkgVertexProperties := getPackageQueryValues(&depPkg)
+	dependencyEdgeProperties := getDependencyQueryValues(&pkg, &depPkg, &dependency)
+	dependencyEdgeProperties[gremlingo.Direction.In] = gremlingo.Merge.InV
+	dependencyEdgeProperties[gremlingo.Direction.Out] = gremlingo.Merge.OutV
+
+	// upsert (pkg -- (dep) --> deppkg)
+	g := gremlingo.Traversal_().WithRemote(c.remote)
+	// FIXME: No usert here, should use v.Has() instead
+	r, err := g.MergeV(pkgVertexProperties).As("pkg").
+		MergeV(depPkgVertexProperties).As("depPkg").
+		MergeE(dependencyEdgeProperties).As("edge").
+		// late bind
+		Option(gremlingo.Merge.InV, gremlingo.T__.Select("pkg")).
+		Option(gremlingo.Merge.OutV, gremlingo.T__.Select("depPkg")).
+		Select("edge").Next()
+	fmt.Printf("MOO upsert: %v %v\n", r, err)
+	if err != nil {
+		return nil, err
+	}
+	id, err := r.GetInt64()
+	if err != nil {
+		return nil, err
+	}
+
+	return getDependencyObject(id, dependencyEdgeProperties), nil
 }
 
 func (c *tinkerpopClient) IngestDependencies(ctx context.Context, pkgs []*model.PkgInputSpec, depPkgs []*model.PkgInputSpec, dependencies []*model.IsDependencyInputSpec) ([]*model.IsDependency, error) {
-	//TODO implement me
-	panic("implement me")
+	// FIXME: Implement bulk insert
+	var isDependencies []*model.IsDependency
+	for k, depSpec := range dependencies {
+		dep, err := c.IngestDependency(ctx, *pkgs[k], *depPkgs[k], *depSpec)
+		if err != nil {
+			return isDependencies, err
+		}
+		isDependencies = append(isDependencies, dep)
+	}
+	return isDependencies, nil
 }
 
 func (c *tinkerpopClient) IsDependency(ctx context.Context, isDependencySpec *model.IsDependencySpec) ([]*model.IsDependency, error) {
